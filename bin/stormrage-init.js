a09a0069d2c7a00d220c9d5dcfb539041d832183
angular-moon <xiaoxuss@163.com>
2019-02-19 18:27:56 +0800

    项目生成后自动启动
    -s 或者 --no-start 参数禁止自动启动

--- bin/stormrage-init.js
+++ bin/stormrage-init.js
@@ -1,3 +1,10 @@
+#!/usr/bin/env node
+
+"use strict";
+
+const program = require("commander");
 const init = require("../lib/commands/init");
 
-init();
+program.option("-s, --no-start", "生成后不自动启动项目").parse(process.argv);
+
+init(program.start);
--- bin/stormrage.js
+++ bin/stormrage.js
@@ -7,6 +7,5 @@ const packageJson = require("../package.json");
 
 program
   .version(packageJson.version, "-v, --version")
-  .command("init", "根据模板生成项目 https://github.com/gmsoft-happyCoding");
-
-program.parse(process.argv);
+  .command("init", "根据模板生成项目 https://github.com/gmsoft-happyCoding")
+  .parse(process.argv);
