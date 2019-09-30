ccf1bc63dd8c6062f4c033a53ab5114bf12e834a
angular-moon <xiaoxuss@163.com>
2019-09-30 15:19:35 +0800

    add exitCode

--- bin/utils/pluginOptionProcess.js
+++ bin/utils/pluginOptionProcess.js
@@ -1,9 +1,11 @@
+const errorCode = require('../../lib/errorCode');
+
 function pluginOptionProcess(option) {
   try {
     return JSON.parse(option);
   } catch (e) {
     console.error('解析 plugin-option 失败, 请检查校验是否是合法的json字符串');
-    throw e;
+    process.exit(errorCode.PLUGIN_OPTION_ERROR);
   }
 }
 
