c9f1a283e34b5a030d378f0925a73664e7e51865
angular-moon <xiaoxuss@163.com>
2020-07-29 10:27:44 +0800

    默认的模板源换回 GitHub

--- bin/stormrage-init.js
+++ bin/stormrage-init.js
@@ -7,7 +7,7 @@ const init = require('../lib/commands/init');
 
 program
   .option('-s, --no-start', '生成后不自动启动项目')
-  .option('-r, --repository-owner <repositoryOwner>', '模板所在仓库前缀')
+  .option('-r, --repository-owner <repositoryOwner>', '模板所在仓库前缀 eg: gitlab:angular-moon')
   .parse(process.argv);
 
 init(program.start, program.repositoryOwner);
