a14eb191864d9c866d1d20c12033f51e5b85589c
angular-moon <xiaoxuss@163.com>
2021-03-11 16:02:59 +0800

    init 支持自定义模板

--- bin/stormrage-init.js
+++ bin/stormrage-init.js
@@ -8,6 +8,10 @@ const init = require('../lib/commands/init');
 program
   .option('-s, --no-start', '生成后不自动启动项目')
   .option('-r, --repository-owner <repositoryOwner>', '模板所在仓库前缀 eg: gitlab:angular-moon')
+  .option(
+    '-t, --template-repo <templateRepo>',
+    '自定义的模板地址(自定义模板不能改变项目配置文件的目录结构), 地址格式参考: https://www.npmjs.com/package/download-git-repo'
+  )
   .parse(process.argv);
 
-init(program.start, program.repositoryOwner);
+init(program.start, program.repositoryOwner, program.templateRepo);
