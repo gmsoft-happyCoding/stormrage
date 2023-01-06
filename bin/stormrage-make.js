084344e766631c1aa7de7266bb08a0b4f5951b38
juilletVent <julysunshine0914@gmail.com>
2023-01-06 23:10:15 +0800

    feat：Make指令完善

--- bin/stormrage-make.js
+++ bin/stormrage-make.js
@@ -6,6 +6,18 @@ const make = require('../lib/commands/make');
 program
   .argument('[localDir]', '（可选）要打包的本地路径，默认为当前执行目录')
   .option('-p, --package <package>', 'react项目为mono仓库时, 指定要发布的package')
+  .option(
+    '-e, --env <env>',
+    '打包所使用的环境变量参考哪个环境去构建，目前默认使用test1的环境变量去构建\n如果有特殊需求，需要使用其他环境的配置进行构建可以使用此参数指定，\n一般情况下请遵守相关规范创建test1的环境变量配置，不要使用本参数'
+  )
+  .option(
+    '-c, --conf <conf>',
+    '打包所使用的环境变量yml文件名称（对yml后缀名不敏感，自适配），如果有特殊情况，\n需要指定配置文件名称，可以使用此参数进行配置，比如同项目发布到私有机房的定制化\n版本，除此之外请遵守配置文件命名规范，不要使用本参数'
+  )
+  .option(
+    '-f, --field <field>',
+    '从yml文件中注入到环境变量中的自定义配置段，CLI会将：business,hosts,gateway,\npdf-preview 这四个配置段进行注入，如果你需要额外的其他配置端，可以使用此参数指\n定，非特殊情况请将业务配置参数定义在business下，遵守字段规范，不要使用本参数\n样例（多个额外字段使用逗号进行分隔）：extra-params-a,extra-params-b'
+  )
   .option('-d, --dest [path]', '标识此次为本地Make，path为发布结果存放位置, 默认为<d:/发布结果>')
   .option('--svn <url>', '通过svn url指定需要发布的项目, 优先级高于 [project]')
   .option('--svn-checkout-dir <path>', 'svn拉取的源代码存放的地址, 默认为用户目录')
@@ -16,7 +28,7 @@ program
   .action((localDir, opts) => {
     make({
       localDir,
-      opts: { ...opts, dest: opts.dest ?? 'D:\\发布结果' },
+      opts,
     });
   })
   .parse(process.argv);
