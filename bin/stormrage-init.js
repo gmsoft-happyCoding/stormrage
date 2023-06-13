e98143bba6f8f61120aaa751ffd5c991e3db1207
和风柒月 <julysunshine0914@gmail.com>
2023-06-13 20:08:09 +0800

    fix：deploy、init指令BUG修正

--- bin/stormrage-init.js
+++ bin/stormrage-init.js
@@ -4,6 +4,7 @@
 
 const program = require('commander');
 const init = require('../lib/commands/init');
+const { ErrorHelper } = require('../lib/utils/ErrorHelper');
 
 program
   .option('-s, --no-start', '生成后不自动启动项目')
@@ -12,6 +13,16 @@ program
     '-t, --template-repo <templateRepo>',
     '自定义的模板地址(自定义模板不能改变项目配置文件的目录结构), 地址格式参考: https://www.npmjs.com/package/download-git-repo'
   )
+  .action(async opts => {
+    try {
+      await init(opts.start, opts.repositoryOwner, opts.templateRepo);
+    } catch (error) {
+      console.error(
+        '[ERROR]: %s\nStack:%s',
+        ErrorHelper.getErrorMessage(error.message, 'make'),
+        error.stack
+      );
+      process.exit(1);
+    }
+  })
   .parse(process.argv);
-
-init(program.start, program.repositoryOwner, program.templateRepo);
